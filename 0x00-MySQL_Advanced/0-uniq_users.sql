d6bcd140bcb78dfd9de750e0bf1fde92d6b68817
kevkatam <kevkatam@gmail.com>
2024-03-21 12:52:33 +0300

    modified 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
---SQL script that creates a table users
+-- sql script that creates a table users
 CREATE TABLE IF NOT EXISTS users (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
        	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
-	);
+	name VARCHAR(255)
+);	
