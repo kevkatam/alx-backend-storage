7fafa773b8088120d796a55c962377ef9766e50f
kevkatam <kevkatam@gmail.com>
2024-03-27 01:34:10 +0300

    adding 9-insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+module that inserts a new document in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts a new document in a collection based on kwargs """
+    
+  
+    mongo_collection.insert_one(kwargs)
